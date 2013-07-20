9c0ac18f7cfb08dd154633d8fc2da1b5ea7c26d9
devel <devel@Dev-Juh>
2013-07-20 09:36:29 -0300

    alterado para mysql

--- src/main/java/br/com/yanaga/jsf_webflow_spring_querydsl_jpa/configuration/root/database/DesenvolvimentoDatabaseConfig.java
+++ src/main/java/br/com/yanaga/jsf_webflow_spring_querydsl_jpa/configuration/root/database/DesenvolvimentoDatabaseConfig.java
@@ -1,45 +1,42 @@
 package br.com.yanaga.jsf_webflow_spring_querydsl_jpa.configuration.root.database;
 
+import br.com.yanaga.jsf_webflow_spring_querydsl_jpa.configuration.root.annotation.Desenvolvimento;
 import javax.sql.DataSource;
-
 import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
-import br.com.yanaga.jsf_webflow_spring_querydsl_jpa.configuration.root.annotation.Desenvolvimento;
-
 @Desenvolvimento
 @Configuration
-public class DesenvolvimentoDatabaseConfig implements DatabaseConfig {
+public class DesenvolvimentoDatabaseConfig implements DatabaseConfig{
 
-	@Bean
-	@Override
-	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-		LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
-		factoryBean.setDataSource(dataSource());
-		factoryBean.setPersistenceUnitName("oracle");
-		return factoryBean;
-	}
+    @Bean
+    @Override
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
+        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+        factoryBean.setDataSource(dataSource());
+        factoryBean.setPersistenceUnitName("mysql");
+        return factoryBean;
+    }
 
-	@Bean
-	@Override
-	public DataSource dataSource() {
-		BasicDataSource dataSource = new BasicDataSource();
-		dataSource.setDriverClassName("oracle.jdbc.driver.OracleDriver");
-		dataSource.setUrl("jdbc:oracle:thin:@192.168.1.112:1521:orcl11g");
-		dataSource.setUsername("maxi");
-		dataSource.setPassword("maxi");
-		dataSource.setMaxActive(100);
-		dataSource.setMaxIdle(10);
-		dataSource.setValidationQuery("SELECT 1 FROM DUAL");
-		dataSource.setTestOnBorrow(true);
-		dataSource.setTestOnReturn(true);
-		dataSource.setTestWhileIdle(true);
-		dataSource.setTimeBetweenEvictionRunsMillis(1800000);
-		dataSource.setNumTestsPerEvictionRun(3);
-		dataSource.setMinEvictableIdleTimeMillis(1800000);
-		return dataSource;
-	}
-
+    @Bean
+    @Override
+    public DataSource dataSource(){
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/webflow");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
+        dataSource.setMaxActive(100);
+        dataSource.setMaxIdle(10);
+        dataSource.setValidationQuery("SELECT 1");
+        dataSource.setTestOnBorrow(true);
+        dataSource.setTestOnReturn(true);
+        dataSource.setTestWhileIdle(true);
+        dataSource.setTimeBetweenEvictionRunsMillis(1800000);
+        dataSource.setNumTestsPerEvictionRun(3);
+        dataSource.setMinEvictableIdleTimeMillis(1800000);
+        return dataSource;
+    }
 }
